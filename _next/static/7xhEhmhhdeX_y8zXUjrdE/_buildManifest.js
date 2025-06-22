39235f12a1cb2be0988e2b8d000ab4d7425c74c9
Anamika Rajendran <arajen97@nmsu.edu>
2025-06-22 01:17:08 -0600

    Updates

---
+++ _next/static/7xhEhmhhdeX_y8zXUjrdE/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/706-1dacca2e4cdfbb5e.js","static/chunks/pages/index-e9c805ec8b93e37b.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
